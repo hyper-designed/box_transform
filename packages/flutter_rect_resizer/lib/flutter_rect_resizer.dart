f6625f6312b2261696ebd9695db812470cf51073
birjuvachhani <brvachhani@gmail.com>
2023-02-02 00:27:07 +0530

    :tada: Initial commit for flutter_rect_resizer

---
+++ packages/flutter_rect_resizer/lib/flutter_rect_resizer.dart
@@ -0,0 +1,4 @@
+library flutter_rect_resizer;
+
+// ignore: invalid_export_of_internal_element
+export 'package:rect_resizer/rect_resizer.dart';
---
+++ packages/flutter_rect_resizer/lib/src/flutter_rect_resizer.dart
@@ -0,0 +1,4 @@
+import 'package:rect_resizer/rect_resizer.dart' as resizer;
+class Resizer extends resizer.Resizer {
+
+}
