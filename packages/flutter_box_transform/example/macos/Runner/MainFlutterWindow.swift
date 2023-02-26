d190003266b3c8358941c7d38a2b812eaa80cfe1
birjuvachhani <brvachhani@gmail.com>
2023-02-26 14:07:46 +0530

    Remove tool bar from macOS window.

--- packages/flutter_box_transform/example/macos/Runner/MainFlutterWindow.swift
+++ packages/flutter_box_transform/example/macos/Runner/MainFlutterWindow.swift
@@ -10,6 +10,27 @@ class MainFlutterWindow: NSWindow {
 
     RegisterGeneratedPlugins(registry: flutterViewController)
 
+    if #available(macOS 10.13, *) {
+        // set defult tool bar for better spacing.
+        self.toolbar = NSToolbar()
+        self.toolbar?.displayMode = NSToolbar.DisplayMode.iconOnly
+        // self.toolbar?.isVisible = false
+
+        var localStyle = self.styleMask;
+        localStyle.insert(.fullSizeContentView)
+        self.styleMask = localStyle;
+        self.titlebarAppearsTransparent = true
+        self.titleVisibility = .hidden
+        self.isOpaque = false
+        self.isMovable = true
+        self.setIsZoomed(true)
+
+        // Disable full screen button.
+        // let button = self.standardWindowButton(NSWindow.ButtonType.zoomButton)
+        // button?.isEnabled = false
+
+    }
+
     super.awakeFromNib()
   }
 }
