e3a6139c4630b303c464c5153975eb750f5fa6f1
birjuvachhani <brvachhani@gmail.com>
2023-04-30 17:40:00 +0530

    Resizing refactor #7
    - Add more tests.

---
+++ packages/box_transform/test/rect_result_test.dart
@@ -0,0 +1,102 @@
+import 'package:box_transform/box_transform.dart';
+import 'package:test/test.dart';
+import 'package:vector_math/vector_math.dart';
+
+void main() {
+  test('RawTransformResult equality tests', (){
+    final result1 = RawTransformResult(
+      rect: Box.fromLTRB(100, 100, 500, 400),
+      handle: HandlePosition.bottomRight,
+      flip: Flip.none,
+      maxHeightReached: false,
+      maxWidthReached: false,
+      minHeightReached: false,
+      minWidthReached: false,
+      oldRect: Box.fromLTRB(100, 100, 300, 200),
+      resizeMode: ResizeMode.freeform,
+      largestRect: Box.fromLTRB(100, 100, 1000, 1000),
+      rawSize: Dimension(400, 300),
+      delta: Vector2(200, 200),
+    );
+
+    final result2 = RawTransformResult(
+      rect: Box.fromLTRB(100, 100, 500, 400),
+      handle: HandlePosition.bottomRight,
+      flip: Flip.none,
+      maxHeightReached: false,
+      maxWidthReached: false,
+      minHeightReached: false,
+      minWidthReached: false,
+      oldRect: Box.fromLTRB(100, 100, 300, 200),
+      resizeMode: ResizeMode.freeform,
+      largestRect: Box.fromLTRB(100, 100, 1000, 1000),
+      rawSize: Dimension(400, 300),
+      delta: Vector2(200, 200),
+    );
+
+    expect(result1, result2);
+    expect(result1.toString(), result2.toString());
+    expect(result1.hashCode, result2.hashCode);
+    expect(result1 == result2, isTrue);
+  });
+
+  test('RawResizeResult equality tests', (){
+    final result1 = RawResizeResult(
+      rect: Box.fromLTRB(100, 100, 500, 400),
+      handle: HandlePosition.bottomRight,
+      flip: Flip.none,
+      maxHeightReached: false,
+      maxWidthReached: false,
+      minHeightReached: false,
+      minWidthReached: false,
+      oldRect: Box.fromLTRB(100, 100, 300, 200),
+      resizeMode: ResizeMode.freeform,
+      largestRect: Box.fromLTRB(100, 100, 1000, 1000),
+      rawSize: Dimension(400, 300),
+      delta: Vector2(200, 200),
+    );
+
+    final result2 = RawResizeResult(
+      rect: Box.fromLTRB(100, 100, 500, 400),
+      handle: HandlePosition.bottomRight,
+      flip: Flip.none,
+      maxHeightReached: false,
+      maxWidthReached: false,
+      minHeightReached: false,
+      minWidthReached: false,
+      oldRect: Box.fromLTRB(100, 100, 300, 200),
+      resizeMode: ResizeMode.freeform,
+      largestRect: Box.fromLTRB(100, 100, 1000, 1000),
+      rawSize: Dimension(400, 300),
+      delta: Vector2(200, 200),
+    );
+
+    expect(result1, result2);
+    expect(result1.toString(), result2.toString());
+    expect(result1.hashCode, result2.hashCode);
+    expect(result1 == result2, isTrue);
+  });
+
+  test('RawMoveResult equality tests', (){
+    final result1 = RawMoveResult(
+      rect: Box.fromLTRB(100, 100, 500, 400),
+      oldRect: Box.fromLTRB(100, 100, 300, 200),
+      largestRect: Box.fromLTRB(100, 100, 1000, 1000),
+      rawSize: Dimension(400, 300),
+      delta: Vector2(200, 200),
+    );
+
+    final result2 = RawMoveResult(
+      rect: Box.fromLTRB(100, 100, 500, 400),
+      oldRect: Box.fromLTRB(100, 100, 300, 200),
+      largestRect: Box.fromLTRB(100, 100, 1000, 1000),
+      rawSize: Dimension(400, 300),
+      delta: Vector2(200, 200),
+    );
+
+    expect(result1, result2);
+    expect(result1.toString(), result2.toString());
+    expect(result1.hashCode, result2.hashCode);
+    expect(result1 == result2, isTrue);
+  });
+}
